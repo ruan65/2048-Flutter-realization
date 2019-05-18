e79af891ea60fd2f3a73d16dbda53bde4188b915
a <ruan65@gmail.com>
2019-05-18 21:23:39 +0300

    tiles

--- lib/model/model.dart
+++ lib/model/model.dart
@@ -1,9 +1,48 @@
+import 'dart:math';
+
 class Board {
   final int row;
   final int column;
   int score;
 
   Board(this.row, this.column);
+
+  List<List<Tile>> _tiles;
+
+  void initTiles() {
+    _tiles = List.generate(
+        4,
+        (row) => List.generate(
+            4,
+            (col) => Tile(
+                  row: row,
+                  column: col,
+                  isNew: false,
+                  canMerge: false,
+                )));
+    score = 0;
+  }
+
+  Tile getTile(int r, int c) => _tiles[r][c];
+
+  void randomEmptyTile() {
+    List<Tile> empty = [];
+    _tiles.forEach((row) {
+      empty.addAll(row.where((tile) => tile.isEmpty()));
+    });
+
+    if(empty.isEmpty) return;
+
+    Random rnd = Random();
+
+    for(int i; i < 4; i++) {
+      int index = rnd.nextInt(empty.length);
+      empty[index].value = rnd.nextInt(9) == 0 ? 4 : 2;
+      empty[index].isNew = true;
+      empty.removeAt(index);
+    }
+
+  }
 }
 
 class Tile {
@@ -14,12 +53,11 @@ class Tile {
 
   Tile({this.row, this.value = 0, this.column, this.canMerge, this.isNew});
 
-  bool isEmpty () =>  value == 0;
+  bool isEmpty() => value == 0;
 
   @override
   int get hashCode => value.hashCode;
 
   @override
   operator ==(other) => other is Tile && value == other.value;
-
 }
