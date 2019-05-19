52c07a9e5afd7a92fb338a2be637cd3d85899d7c
a <ruan65@gmail.com>
2019-05-19 13:10:23 +0300

    moving and merging

--- lib/model/model.dart
+++ lib/model/model.dart
@@ -31,17 +31,166 @@ class Board {
       empty.addAll(row.where((tile) => tile.isEmpty()));
     });
 
-    if(empty.isEmpty) return;
+    if (empty.isEmpty) return;
 
     Random rnd = Random();
 
-    for(int i; i < 4; i++) {
+    for (int i; i < 4; i++) {
       int index = rnd.nextInt(empty.length);
       empty[index].value = rnd.nextInt(9) == 0 ? 4 : 2;
       empty[index].isNew = true;
       empty.removeAt(index);
     }
+  }
 
+  void mergeLeft(int row, int col) {
+    while (col > 0) {
+      merge(_tiles[row][col - 1], _tiles[row][col]);
+      col--;
+    }
+  }
+
+  void mergeRight(int row, int col) {
+    while (col < column - 1) {
+      merge(_tiles[row][col + 1], _tiles[row][col]);
+      col++;
+    }
+  }
+
+  void mergeUp(int row, int col) {
+    while (row > 0) {
+      merge(_tiles[row - 1][col], _tiles[row][col]);
+      row--;
+    }
+  }
+
+  void mergeDown(int row, int col) {
+    while (row < this.row - 1) {
+      merge(_tiles[row + 1][col], _tiles[row][col]);
+      row++;
+    }
+  }
+
+  void moveLeft() {
+    if (!canMoveLeft()) return;
+
+    for (int r = 0; r < row; r++) {
+      for (int c = 1; c < column; c++) {
+        mergeLeft(r, c);
+      }
+    }
+    randomEmptyTile();
+    resetCamMerge();
+  }
+
+  void moveRight() {
+    if (!canMoveRight()) return;
+
+    for (int r = 0; r < row; r++) {
+      for (int c = column -1; c > 0; c--) {
+        mergeRight(r, c);
+      }
+    }
+    randomEmptyTile();
+    resetCamMerge();
+  }
+
+  void moveUp() {
+    if (!canMoveUp()) return;
+
+    for (int r = 1; r < row; r++) {
+      for (int c = 0; c < column; c++) {
+        mergeUp(r, c);
+      }
+    }
+    randomEmptyTile();
+    resetCamMerge();
+  }
+
+  void moveDown() {
+    if (!canMoveDown()) return;
+
+    for (int r = row - 1; r > 0; r--) {
+      for (int c = 0; c < column; c++) {
+        mergeDown(r, c);
+      }
+    }
+    randomEmptyTile();
+    resetCamMerge();
+  }
+
+  bool canMoveLeft() {
+    for (int r = 0; r < row; r++) {
+      for (int c = 1; c < column; c++) {
+        if (canMerge(_tiles[r][c], _tiles[r][c - 1])) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  bool canMoveRight() {
+    for (int r = 0; r < row; r++) {
+      for (int c = column - 1; c > 0; c--) {
+        if (canMerge(_tiles[r][c], _tiles[r][c + 1])) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  bool canMoveUp() {
+    for (int r = 1; r < row; r++) {
+      for (int c = 0; c < column; c++) {
+        if (canMerge(_tiles[r - 1][c], _tiles[r][c])) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  bool canMoveDown() {
+    for (int r = row - 1; r > 0; r--) {
+      for (int c = 0; c < column; c++) {
+        if (canMerge(_tiles[r][c], _tiles[r + 1][c])) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  bool canMerge(Tile a, Tile b) =>
+      !a.canMerge &&
+      ((b.isEmpty() && !a.isEmpty()) && (!a.isEmpty() && a == b));
+
+  void resetCamMerge() {
+    _tiles.forEach((col) {
+      col.forEach((t) => t.canMerge = false);
+    });
+  }
+
+  void merge(Tile a, Tile b) {
+    if (!canMerge(a, b)) {
+      if (!a.isEmpty() && !b.canMerge) {
+        b.canMerge = true;
+      }
+      return;
+    }
+
+    if (b.isEmpty()) {
+      b.value = a.value;
+      a.value = 0;
+    } else if (a == b) {
+      b.value = b.value * 2;
+      a.value = 0;
+      b.canMerge = true;
+    } else {
+      b.canMerge = true;
+    }
   }
 }
 
